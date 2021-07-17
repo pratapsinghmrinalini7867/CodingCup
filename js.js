3e952fdf5c36317f57fb4df80b3097a3a3c6576e
unknown <pratapsinghmrinalini7867@gmail.com>
2021-07-17 15:43:01 +0530

    codingcup

---
+++ js.js
@@ -0,0 +1,51 @@
+var codeSol = document.querySelector("#code");
+var solution = document.querySelector(".solution");
+var codeSol2 = document.querySelector("#code-2");
+var solution2 = document.querySelector(".solution2");
+var codeSol3 = document.querySelector("#code-3");
+var solution3 = document.querySelector(".solution3");
+var codeSol4 = document.querySelector("#code-4");
+var solution4 = document.querySelector(".solution4");
+var codeSol5 = document.querySelector("#code-5");
+var solution5 = document.querySelector(".solution5");
+var codeSol6 = document.querySelector("#code-6");
+var solution6 = document.querySelector(".solution6");
+var codeSol7 = document.querySelector("#code-7");
+var solution7 = document.querySelector(".solution7");
+var codeSol8 = document.querySelector("#code-8");
+var solution8 = document.querySelector(".solution8");
+var codeSol9 = document.querySelector("#code-9");
+var solution9 = document.querySelector(".solution9");
+var codeSol10 = document.querySelector("#code-10");
+var solution10 = document.querySelector(".solution10");
+
+codeSol.addEventListener("click",function(){
+    solution.classList.toggle("solution");
+});
+codeSol2.addEventListener("click",function(){
+  solution2.classList.toggle("solution2");
+});
+codeSol3.addEventListener("click",function(){
+  solution3.classList.toggle("solution3");
+});
+codeSol4.addEventListener("click",function(){
+  solution4.classList.toggle("solution4");
+});
+codeSol5.addEventListener("click",function(){
+  solution5.classList.toggle("solution5");
+});
+codeSol6.addEventListener("click",function(){
+  solution6.classList.toggle("solution6");
+});
+codeSol7.addEventListener("click",function(){
+  solution7.classList.toggle("solution7");
+});
+codeSol8.addEventListener("click",function(){
+  solution8.classList.toggle("solution8");
+});
+codeSol9.addEventListener("click",function(){
+  solution9.classList.toggle("solution9");
+});
+codeSol10.addEventListener("click",function(){
+  solution10.classList.toggle("solution10");
+});
